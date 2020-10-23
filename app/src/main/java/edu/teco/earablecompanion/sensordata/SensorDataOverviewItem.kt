03cfc84952b0a74fedfe60e3ded229415b407ed0
Felix <felixschmidbb@gmail.com>
2020-10-23 18:35:08 +0200

    Add stoppedAt to overview, fix margins and chart icon

--- app/src/main/java/edu/teco/earablecompanion/sensordata/SensorDataOverviewItem.kt
+++ app/src/main/java/edu/teco/earablecompanion/sensordata/SensorDataOverviewItem.kt
@@ -7,14 +7,15 @@ import java.time.LocalDateTime
 sealed class SensorDataOverviewItem {
     object NoData : SensorDataOverviewItem()
 
-    data class Data(val id: Long, val title: String, val description: String, val createdAt: LocalDateTime, val duration: Duration?, val entryCount: Int) : SensorDataOverviewItem() {
+    data class Data(val id: Long, val title: String, val description: String, val createdAt: LocalDateTime, val stoppedAt: LocalDateTime?, val duration: Duration?, val entryCount: Int) : SensorDataOverviewItem() {
 
         companion object {
             fun SensorData.toOverviewItem(entryCount: Int) = Data(
                 id = dataId,
                 title = title,
-                createdAt = createdAt,
                 description = description ?: "",
+                createdAt = createdAt,
+                stoppedAt = stoppedAt,
                 duration = stoppedAt?.let { Duration.between(createdAt, stoppedAt) },
                 entryCount = entryCount
             )
